1d354a723f573da2796ef800639d057405e4427e
Ethan Lane <ethan@vylpes.com>
2022-05-11 17:51:21 +0100

    Add bunny command back

---
+++ src/commands/bunny.ts
@@ -0,0 +1,29 @@
+import ErrorEmbed from "../helpers/embeds/ErrorEmbed";
+import PublicEmbed from "../helpers/embeds/PublicEmbed";
+import { Command } from "../type/command";
+import { ICommandContext } from "../contracts/ICommandContext";
+import randomBunny from "random-bunny";
+
+export default class Bunny extends Command {
+    constructor() {
+        super();
+
+        super.Category = "Fun";
+    }
+
+    public override async execute(context: ICommandContext) {
+        const result = await randomBunny('rabbits', 'hot');
+
+        if (result.IsSuccess) {
+            const embed = new PublicEmbed(context, result.Result!.Title, "")
+                .setImage(result.Result!.Url)
+                .setURL(`https://reddit.com${result.Result!.Permalink}`)
+                .setFooter({ text: `r/Rabbits · ${result.Result!.Ups} upvotes` });
+            
+            embed.SendToCurrentChannel();
+        } else {
+            const errorEmbed = new ErrorEmbed(context, "There was an error using this command.");
+            errorEmbed.SendToCurrentChannel();
+        }
+    }
+}
