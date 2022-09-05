2da5e1aa75aa039d77bf148ad260821a9644a0c0
Vylpes <ethan@vylpes.com>
2022-09-05 18:07:32 +0100

    Feature/vba 52 (#175)
    * Add say command (#174)
    
    Co-authored-by: Ethan Lane <ethan@vylpes.com>
    Reviewed-on: https://gitea.vylpes.xyz/RabbitLabs/vylbot-app/pulls/174
    
    * Add repo and funding link to about message (#176)
    
    Reviewed-on: https://gitea.vylpes.xyz/RabbitLabs/vylbot-app/pulls/176
    
    * Add other subreddits to bunny command
    
    * Fix changes requested

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -1,3 +1,5 @@
+import { Emoji, MessageActionRow, MessageButton } from "discord.js";
+import { MessageButtonStyles } from "discord.js/typings/enums";
 import { ICommandContext } from "../contracts/ICommandContext";
 import PublicEmbed from "../helpers/embeds/PublicEmbed";
 import { Command } from "../type/command";
@@ -9,11 +11,32 @@ export default class About extends Command {
     }
 
     public override async execute(context: ICommandContext) {
+        const fundingLink = process.env.ABOUT_FUNDING;
+        const repoLink = process.env.ABOUT_REPO;
+
         const embed = new PublicEmbed(context, "About", "")
-            .addField("Version", process.env.BOT_VER!)
-            .addField("Author", process.env.BOT_AUTHOR!)
-            .addField("Date", process.env.BOT_DATE!);
+            .addField("Version", process.env.BOT_VER!, true)
+            .addField("Author", process.env.BOT_AUTHOR!, true)
+            .addField("Date", process.env.BOT_DATE!, true);
+
+        const row = new MessageActionRow();
+
+        if (repoLink) {
+            row.addComponents(
+                new MessageButton()
+                    .setURL(repoLink)
+                    .setLabel("Repo")
+                    .setStyle(MessageButtonStyles.LINK));
+        }
+
+        if (fundingLink) {
+            row.addComponents(
+                new MessageButton()
+                    .setURL(fundingLink)
+                    .setLabel("Funding")
+                    .setStyle(MessageButtonStyles.LINK));
+        }
         
-        await embed.SendToCurrentChannel();
+        await embed.SendToCurrentChannel({ components: [row] });
     }
 }
