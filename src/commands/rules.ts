c4b5d9671b7c143b700bce685e24694ef317fd5b
Ethan Lane <ethan@vylpes.com>
2022-12-12 18:11:16 +0000

    Add ephemeral reply upon rules success

--- src/commands/rules.ts
+++ src/commands/rules.ts
@@ -32,13 +32,13 @@ export default class Rules extends Command {
         const rules = JSON.parse(rulesFile) as IRules[];
 
         const embeds: EmbedBuilder[] = [];
-        
+
         rules.forEach(rule => {
             const embed = new EmbedBuilder()
                 .setColor(EmbedColours.Ok)
                 .setTitle(rule.title || "Rules")
                 .setDescription(rule.description ? rule.description.join("\n") : "*none*");
-            
+
             if (rule.image) {
                 embed.setImage(rule.image);
             }
@@ -55,7 +55,14 @@ export default class Rules extends Command {
         if (!channel) {
             return;
         }
-        
+
         await channel.send({ embeds: embeds });
+
+        const successEmbed = new EmbedBuilder()
+            .setColor(EmbedColours.Ok)
+            .setTitle("Success")
+            .setDescription("The rules have sent to this channel successfully");
+
+        await interaction.reply({ embeds: [ successEmbed], ephemeral: true });
     }
 }
