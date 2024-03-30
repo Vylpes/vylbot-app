2b04952d85bbe9d728c1bde7d8a10b0d1f3f1955
Ethan Lane <ethan@vylpes.com>
2024-03-30 16:15:53 +0000

    Update cache helper using the wrong symbol to measure interval

--- src/helpers/CacheHelper.ts
+++ src/helpers/CacheHelper.ts
@@ -2,10 +2,10 @@ import { Guild } from "discord.js";
 import Server from "../database/entities/Server";
 
 export default class CacheHelper {
-    public static async UpdateServerCache(guild: Guild) {
+    public static async UpdateServerCache(guild: Guild): Promise<boolean> {
         const cacheInterval = process.env.CACHE_INTERVAL;
 
-        if (!cacheInterval) return;
+        if (!cacheInterval) return false;
 
         let server = await Server.FetchOneById(Server, guild.id);
 
@@ -14,9 +14,15 @@ export default class CacheHelper {
             await server.Save(Server, server);
 
             await CacheHelper.UpdateCache(guild);
-        } else if (server.LastCached.getTime() + Number(cacheInterval) < Date.now()) {
+
+            return true;
+        } else if (server.LastCached.getTime() + Number(cacheInterval) > Date.now()) {
             await CacheHelper.UpdateCache(guild);
+
+            return true;
         }
+
+        return false;
     }
 
     private static async UpdateCache(guild: Guild) {
