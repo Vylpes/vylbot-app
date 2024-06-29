451c53f2cd966a0c9b211568599bf810157e4241
Ethan Lane <ethan@vylpes.com>
2024-06-29 09:26:17 +0100

    WIP: Start of basentity tests

--- tests/contracts/BaseEntity.test.ts
+++ tests/contracts/BaseEntity.test.ts
@@ -1,5 +1,29 @@
+import BaseEntity from "../../src/contracts/BaseEntity";
+import uuid from "uuid";
+
+jest.mock("uuid", () => {
+    return {
+        v4: () => "uuidv4",
+    };
+});
+
+jest.useFakeTimers();
+
 describe('constructor', () => {
-    test.todo("EXPECT properties to be set");
+    test("EXPECT properties to be set", () => {
+        // Arrange
+        const systemTime = new Date("2024-06-29T00:00:00.000Z");
+
+        jest.setSystemTime(systemTime);
+
+        // Act
+        const entity = new BaseEntity();
+
+        // Assert
+        expect(entity.Id).toBe("uuidv4");
+        expect(entity.WhenCreated).toStrictEqual(systemTime);
+        expect(entity.WhenUpdated).toStrictEqual(systemTime);
+    });
 });
 
 describe("Save", () => {
