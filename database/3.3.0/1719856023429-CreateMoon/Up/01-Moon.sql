674a4ac614fee7b2155d335b86f1e402ceadd6b1
Ethan Lane <ethan@vylpes.com>
2024-08-17 16:54:11 +0100

    Fix typo in moon sql script

--- database/3.3.0/1719856023429-CreateMoon/Up/01-Moon.sql
+++ database/3.3.0/1719856023429-CreateMoon/Up/01-Moon.sql
@@ -4,7 +4,7 @@ CREATE TABLE `moon` (
   `WhenUpdated` datetime NOT NULL,
   `MoonNumber` int NOT NULL,
   `UserId` varchar(255) NOT NULL,
-  `Description` varchar(255) NOT NULL
-  `WhenArchived` datetime NULL,
+  `Description` varchar(255) NOT NULL,
+  `WhenArchived` datetime NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
