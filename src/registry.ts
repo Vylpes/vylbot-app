891bab5a04632342c4c0e947c0e35b9ed567f7e6
Ethan Lane <ethan@vylpes.com>
2025-11-25 18:50:19 +0000

    Remove moons code (#517)
    #516
    
    Reviewed-on: https://git.vylpes.xyz/RabbitLabs/vylbot-app/pulls/517
    Reviewed-by: VylpesTester <tester@vylpes.com>
    Co-authored-by: Ethan Lane <ethan@vylpes.com>
    Co-committed-by: Ethan Lane <ethan@vylpes.com>

--- src/registry.ts
+++ src/registry.ts
@@ -30,9 +30,6 @@ import AddLobby from "./commands/501231711271780357/Lobby/add";
 import RemoveLobby from "./commands/501231711271780357/Lobby/remove";
 import ListLobby from "./commands/501231711271780357/Lobby/list";
 
-// Command Imports: Potato Talk
-import Moons from "./commands/304276391837302787/moons";
-
 // Event Imports
 import GuildMemberAdd from "./events/MemberEvents/GuildMemberAdd";
 import GuildMemberRemove from "./events/MemberEvents/GuildMemberRemove";
@@ -43,7 +40,6 @@ import MessageCreate from "./events/MessageEvents/MessageCreate";
 
 // Button Event Imports
 import Verify from "./buttonEvents/verify";
-import MoonsButtonEvent from "./buttonEvents/304276391837302787/moons";
 
 export default class Registry {
     public static RegisterCommands() {
@@ -76,16 +72,12 @@ export default class Registry {
         CoreClient.RegisterCommand("listlobby", new ListLobby(), "501231711271780357");
         CoreClient.RegisterCommand("entry", new Entry(), "501231711271780357");
 
-        // Exclusive Commands: Potato Talk
-        CoreClient.RegisterCommand("moons", new Moons(), "304276391837302787");
-
         // Add Exclusive Commands to Test Server
         CoreClient.RegisterCommand("lobby", new Lobby(), "442730357897429002");
         CoreClient.RegisterCommand("addlobby", new AddLobby(), "442730357897429002");
         CoreClient.RegisterCommand("removelobby", new RemoveLobby(), "442730357897429002");
         CoreClient.RegisterCommand("listlobby", new ListLobby(), "442730357897429002");
         CoreClient.RegisterCommand("entry", new Entry(), "442730357897429002");
-        CoreClient.RegisterCommand("moons", new Moons(), "442730357897429002");
     }
 
     public static RegisterEvents() {
@@ -100,6 +92,5 @@ export default class Registry {
 
     public static RegisterButtonEvents() {
         CoreClient.RegisterButtonEvent("verify", new Verify());
-        CoreClient.RegisterButtonEvent("moons", new MoonsButtonEvent());
     }
 }
